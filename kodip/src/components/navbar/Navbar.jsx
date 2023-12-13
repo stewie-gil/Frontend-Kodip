63ceea6a04fd5a8e784393a74f0cd6e9fee8505f
Zeemonk1 <110388816+ZeeMonk-pixel@users.noreply.github.com>
2023-12-13 17:53:14 +0100

    complete layout, navbar and footer

--- kodip/src/components/navbar/Navbar.jsx
+++ kodip/src/components/navbar/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/images/logo.png";
 
 import "./navbar.css";
+import { ArrowDown2 } from "iconsax-react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const navLink = [
+    {
+      name: "About us",
+      link: "",
+      icon: "",
+    },
+    {
+      name: "Article",
+      link: "",
+      icon: "",
+    },
+    {
+      name: "Property",
+      link: "",
+      icon: <ArrowDown2 color="white" size={16} />,
+    },
+    {
+      name: "Sign up",
+      link: "",
+      icon: "",
+    },
+  ];
 
-export default Navbar
+  return (
+    <div className="nav-bar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <h2>Kodip</h2>
+      </div>
+      <div className="nav-links">
+        {navLink.map((item, idx) => {
+          return (
+            <ul key={idx}>
+              <li className={item.name === "Sign up" ? "signup" : undefined}>{item.name} {item.icon}</li>
+            </ul>
+          )
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
